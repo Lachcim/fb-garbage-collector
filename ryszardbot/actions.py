b9546b4129c5d5fc0dd82d9b0594ff25d8c97520
Michał Szopiński <jajnick@hotmail.com>
2020-04-25 11:15:06 +0200

    Add logging

--- ryszardbot/actions.py
+++ ryszardbot/actions.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import logging
 from time import sleep
 from selenium.common.exceptions import TimeoutException
 from ryszardbot.helpers import execute_script
@@ -11,32 +12,32 @@ def remove_failed_posts(self):
     
     for post in posts:
         if post["admin"]:
-            print("post by {0} allowed due to adminship".format(post["author"]))
+            logging.info("post by {0} allowed due to adminship".format(post["author"]))
             continue
             
         if now - post["time"] >= 86400:
-            print("post by {0} grandfathered in".format(post["author"]))
+            logging.info("post by {0} grandfathered in".format(post["author"]))
             continue
             
         if now - post["time"] >= 3600 and post["likeCount"] >= 50:
-            print("post by {0} passed".format(post["author"]))
+            logging.info("post by {0} passed".format(post["author"]))
             continue
             
         if now - post["time"] < 3600:
-            print("post by {0} isn't old enough ({1:.2f} hours)".format(post["author"], (now - post["time"]) / 3600))
+            logging.info("post by {0} isn't old enough ({1:.2f} hours)".format(post["author"], (now - post["time"]) / 3600))
             continue
             
         message = "AUTOMATYCZNE USUNIĘCIE: LICZBA REAKCJI {0} PO {1:.2f} GODZ.\n\n"
         message += "Aby utrzymać wysoki poziom grupy, wpisy które nie osiągnęły 50 reakcji w ciągu godziny są automatycznie usuwane. "
         message += "Usuwanie jest zawieszone w godzinach od 23 do 11."
             
-        print("post by {0} marked for deletion, {1} likes reached in {2:.2f} hours".format(post["author"], post["likeCount"], (now - post["time"]) / 3600))
+        logging.info("post by {0} marked for deletion, {1} likes reached in {2:.2f} hours".format(post["author"], post["likeCount"], (now - post["time"]) / 3600))
         result = self.remove_post(post["permalink"], message.format(post["likeCount"], (now - post["time"]) / 3600))
         
         if result:
-            print("post deleted successfully")
+            logging.info("post deleted successfully")
         else:
-            print("couldn't remove post")
+            logging.info("couldn't remove post")
 
 def remove_post(self, permalink, reason):
     return self.execute_script("deletepost", permalink, reason)
--- testbot.py
+++ testbot.py
@@ -1,11 +1,30 @@
 import code
+import logging
+import sys
 import traceback
 from threading import Thread
 import ryszardbot
 
+# configure logging to file
+logging.basicConfig(
+    handlers=[logging.FileHandler("ryszardbot.log", "a", "utf-8")],
+    format="%(asctime)s %(levelname)s %(message)s",
+    level=logging.INFO)
+
+# also log to stdout
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel("INFO")
+stdout_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+logging.getLogger().addHandler(stdout_handler)
+
+logging.info("bot starting")
 bot = ryszardbot.RyszardBot()
 bot.start_driver()
+
+logging.info("logging in")
 bot.log_in()
+
+logging.info("bot online")
 
 def console_exit():
     raise SystemExit
