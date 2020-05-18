926933be6ad3d1a7d25713e6a0eb6a791765f3c1
Michał Szopiński <jajnick@hotmail.com>
2020-05-18 18:17:11 +0200

    Fix permalink retrieval for shared posts

--- ryszardbot/js/getposts.js
+++ ryszardbot/js/getposts.js
@@ -17,7 +17,7 @@ for (var i = 0; i < rawPosts.length; i++) {
 	posts[i].image = rawPosts[i].querySelector("img[data-src]");
 	if (posts[i].image) posts[i].image = posts[i].image.src;
 	posts[i].admin = rawPosts[i].querySelector("div[data-testid=story-subtitle] a[href*='badge_type=ADMIN']") != null;
-	posts[i].permalink = rawPosts[i].querySelector("a[href*=permalink]").href;
+	posts[i].permalink = rawPosts[i].querySelector("div[data-testid=story-subtitle] a[href*=permalink]").href;
 }
 
 return posts;
