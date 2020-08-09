603e3904f21e72d0ae8ce0fa31cad7a363f001c0
Michał Szopiński <mszopinski@mssnt.pl>
2020-08-09 16:57:48 +0200

    Fix headless bug on Windows

--- fbgc/__init__.py
+++ fbgc/__init__.py
@@ -18,7 +18,7 @@ class FBGarbageCollector:
         options.add_argument("headless")
         options.add_argument("log-level=3")
         options.add_argument("user-agent=Mozilla/5.0 Chrome/80.0 FBGarbageCollector/1.0")
-        options.add_argument("user-data-dir=userdata")
+        options.add_argument("user-data-dir={}".format(os.path.abspath("userdata")))
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
         
         self.driver = webdriver.Chrome(chrome_options=options)
